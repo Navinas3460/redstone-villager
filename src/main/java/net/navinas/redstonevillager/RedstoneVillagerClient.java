ee7df4d6d38347dfed2ed008ad4245a71c6d1d2e
Navinas3460 <navinas3460@gmail.com>
2022-12-17 13:17:21 +0530

    Initial commit

---
+++ src/main/java/net/navinas/redstonevillager/RedstoneVillagerClient.java
@@ -0,0 +1,10 @@
+package net.navinas.redstonevillager;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RedstoneVillagerClient implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
